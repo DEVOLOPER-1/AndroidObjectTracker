afbf817d98d1e5f2c2173de0b2f353cb4743b96a
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2026-05-06 01:06:34 +0300

    Cleansing the branch

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApp) apply false
-    alias(libs.plugins.androidLib) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.composeCompiler) apply false
-}
